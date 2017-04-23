76567ddde37b48070baff86b21da46cdad5ad187
Edipo <edipo2s@gmail.com>
2017-04-23 17:54:57 -0300

    1920x1080 screen and game configs

---
+++ src/main/kotlin/com/ediposouza/util/ReferenceConfig1920x1080.kt
@@ -0,0 +1,153 @@
+package com.ediposouza.util
+
+/**
+ * Created by ediposouza on 16/03/17.
+ */
+class ReferenceConfig1920x1080 : ReferenceConfig() {
+
+    override val SCREEN_REFERENCE_WIDTH = 1920
+    override val SCREEN_REFERENCE_HEIGHT = 1080
+
+    // -- APP --
+
+    override val APP_MAIN_HEIGHT = 105
+    override val APP_MAIN_WIDTH = 105
+    override val APP_MAIN_X = 1800
+    override val APP_MAIN_Y = 970
+
+    override val DECK_TRACKER_WIDTH = 350
+    override val DECK_TRACKER_X = 0
+    override val DECK_TRACKER_Y = 20
+    override val DECK_TRACKER_CARD_HEIGHT = 42
+    override val DECK_TRACKER_CARD_WIDTH = 210
+
+    // -- Screens --
+
+    override val SCREEN_MAIN_HEIGHT = 72
+    override val SCREEN_MAIN_WIDTH = 108
+    override val SCREEN_MAIN_X = 688
+    override val SCREEN_MAIN_Y = 927
+
+    override val SCREEN_MAIN_MODE_HEIGHT = 144
+    override val SCREEN_MAIN_MODE_WIDTH = 184
+    override val SCREEN_MAIN_MODE_X = 562
+    override val SCREEN_MAIN_MODE_Y = 180
+
+    override val SCREEN_GAME_HEIGHT = 65
+    override val SCREEN_GAME_WIDTH = 135
+    override val SCREEN_GAME_X = 1321
+    override val SCREEN_GAME_Y = 905
+
+    override val SCREEN_ARENA_CLASSES_HEIGHT = 170
+    override val SCREEN_ARENA_CLASSES_WIDTH = 113
+    override val SCREEN_ARENA_CLASSES_X = 702
+    override val SCREEN_ARENA_CLASSES_Y = 185
+
+    override val SCREEN_ARENA_PICKS_HEIGHT = 0
+    override val SCREEN_ARENA_PICKS_WIDTH = 0
+    override val SCREEN_ARENA_PICKS_X = 0
+    override val SCREEN_ARENA_PICKS_Y = 0
+
+    override val SCREEN_ARENA_PICK_NUMBER_HEIGHT = 0
+    override val SCREEN_ARENA_PICK_NUMBER_WIDTH = 0
+    override val SCREEN_ARENA_PICK_NUMBER_X = 0
+    override val SCREEN_ARENA_PICK_NUMBER_Y = 0
+
+    override val SCREEN_ARENA_DASHBOARD_HEIGHT = 0
+    override val SCREEN_ARENA_DASHBOARD_WIDTH = 0
+    override val SCREEN_ARENA_DASHBOARD_X = 0
+    override val SCREEN_ARENA_DASHBOARD_Y = 0
+
+    // -- Game --
+
+    override val GAME_WIN_HEIGHT = 98
+    override val GAME_WIN_WIDTH = 98
+    override val GAME_WIN_X = 912
+    override val GAME_WIN_Y = 90
+
+    override val GAME_WIN2_HEIGHT = 555
+    override val GAME_WIN2_WIDTH = 570
+    override val GAME_WIN2_X = 698
+    override val GAME_WIN2_Y = 215
+
+    override val GAME_LOSS_HEIGHT = 120
+    override val GAME_LOSS_WIDTH = 107
+    override val GAME_LOSS_X = 907
+    override val GAME_LOSS_Y = 784
+
+    override val GAME_LOSS2_HEIGHT = 430
+    override val GAME_LOSS2_WIDTH = 430
+    override val GAME_LOSS2_X = 770
+    override val GAME_LOSS2_Y = 345
+
+    override val GAME_CARD_DRAW_HEIGHT = 225
+    override val GAME_CARD_DRAW_WIDTH = 170
+    override val GAME_CARD_DRAW_X = 1409
+    override val GAME_CARD_DRAW_Y = 504
+
+    override val GAME_CARD_DRAW_PROPHECY_HEIGHT = 170
+    override val GAME_CARD_DRAW_PROPHECY_WIDTH = 131
+    override val GAME_CARD_DRAW_PROPHECY_X = 1325
+    override val GAME_CARD_DRAW_PROPHECY_Y = 636
+
+    override val GAME_CARD_GENERATED_HEIGHT = 80
+    override val GAME_CARD_GENERATED_WIDTH = 110
+    override val GAME_CARD_GENERATED_X = 1555
+    override val GAME_CARD_GENERATED_Y = 640
+
+    override val CARD_INITIAL_DRAW_HEIGHT = 225
+    override val CARD_INITIAL_DRAW_WIDTH = 170
+    override val CARD_INITIAL_DRAW_FIRST_X = 580
+    override val CARD_INITIAL_DRAW_SECOND_X = 875
+    override val CARD_INITIAL_DRAW_THIRD_X = 1172
+    override val CARD_INITIAL_DRAW_Y = 324
+
+    override val GAME_PLAYER_FIRST_HEIGHT = 72
+    override val GAME_PLAYER_FIRST_WIDTH = 78
+    override val GAME_PLAYER_FIRST_X = 1362
+    override val GAME_PLAYER_FIRST_Y = 773
+
+    override val GAME_PLAYER_SECOND_HEIGHT = 74
+    override val GAME_PLAYER_SECOND_WIDTH = 76
+    override val GAME_PLAYER_SECOND_X = 1294
+    override val GAME_PLAYER_SECOND_Y = 160
+
+    override val GAME_PLAYER_CLASS_HEIGHT = 48
+    override val GAME_PLAYER_CLASS_WIDTH = 100
+    override val GAME_PLAYER_CLASS_X = 1335
+    override val GAME_PLAYER_CLASS_Y = 992
+
+    override val GAME_OPPONENT_CLASS_HEIGHT = 48
+    override val GAME_OPPONENT_CLASS_WIDTH = 99
+    override val GAME_OPPONENT_CLASS_X = 1283
+    override val GAME_OPPONENT_CLASS_Y = 18
+
+    // -- Arena --
+
+    override val ARENA_PICK_CLASS_HEIGHT = 0
+    override val ARENA_PICK_CLASS_WIDTH = 0
+    override val ARENA_PICK_CLASS_X = 0
+    override val ARENA_PICK_CLASS_Y = 0
+
+    override val ARENA_PICK_CARD_HEIGHT = 0
+    override val ARENA_PICK_CARD_WIDTH = 0
+    override val ARENA_PICK_CARD_START_Y = 0
+    override val ARENA_PICK_CARD_FIRST_X = 0
+    override val ARENA_PICK_CARD_SECOND_X = 0
+    override val ARENA_PICK_CARD_THIRD_X = 0
+
+    override val ARENA_PICK_SELECT_HEIGHT = 0
+    override val ARENA_PICK_SELECT_WIDTH = 0
+    override val ARENA_PICK_SELECT_FIRST_X = 0
+    override val ARENA_PICK_SELECT_SECOND_X = 0
+    override val ARENA_PICK_SELECT_THIRD_X = 0
+    override val ARENA_PICK_SELECT_Y = 0
+
+    override val ARENA_PICK_NUMBER_HEIGHT = 0
+    override val ARENA_PICK_NUMBER_WIDTH = 0
+    override val ARENA_PICK_NUMBER_FIRST_X = 0
+    override val ARENA_PICK_NUMBER_SECOND_X = 0
+    override val ARENA_PICK_NUMBER_THIRD_X = 0
+    override val ARENA_PICK_NUMBER_Y = 0
+
+}
---
+++ src/test/kotlin/com/ediposouza/Tests1920x1080.kt
@@ -0,0 +1,225 @@
+package com.ediposouza
+
+import com.ediposouza.data.DHash
+import com.ediposouza.data.DHashCards
+import com.ediposouza.extensions.*
+import com.ediposouza.util.BaseRecognizeTests
+import com.ediposouza.util.Recognizer
+import com.ediposouza.util.ReferenceConfig1920x1080
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+import java.awt.image.BufferedImage
+
+/**
+ * Created by ediposouza on 06/03/17.
+ */
+class Tests1920x1080 : BaseRecognizeTests() {
+
+    @Before
+    fun setUp() {
+        TESLTracker.referenceConfig = ReferenceConfig1920x1080()
+    }
+
+    @Test
+    fun testScreenMain() {
+        recognizeScreen("ScreenMain.png", DHash.SCREEN_MAIN, BufferedImage::getScreenMainCrop)
+    }
+
+    @Test
+    fun testScreenMainMode() {
+        recognizeScreen("ScreenMainModeCasual.png", DHash.SCREEN_MAIN_MODE_CASUAL, BufferedImage::getScreenMainModeCrop)
+        recognizeScreen("ScreenMainModeRanked.png", DHash.SCREEN_MAIN_MODE_RANKED, BufferedImage::getScreenMainModeCrop)
+        recognizeScreen("ScreenMainModePratice.png", DHash.SCREEN_MAIN_MODE_PRATICE, BufferedImage::getScreenMainModeCrop)
+    }
+
+    @Test
+    fun testScreenGame() {
+        recognizeScreen("Game/PlayFirst.png", DHash.SCREEN_GAME, BufferedImage::getScreenGameCrop)
+    }
+
+    @Test
+    fun testScreenArenaClasses() {
+        recognizeScreen("ScreenArenaClasses.png", DHash.SCREEN_ARENA_CLASSES, BufferedImage::getScreenArenaClassesCrop)
+    }
+
+//    @Test
+//    fun testScreenArenaPicks() {
+//        recognizeScreen("ScreenArenaPicks.png", DHash.SCREEN_ARENA_PICKS, BufferedImage::getScreenArenaPicksCrop)
+//    }
+//
+//    @Test
+//    fun testScreenArenaDash() {
+//        recognizeScreen("ScreenArenaDash.png", DHash.SCREEN_ARENA_DASHBOARD, BufferedImage::getScreenArenaDashboardCrop)
+//    }
+//
+//    @Test
+//    fun testScreenArenaPickFalse() {
+//        recognizePickScreen("ArenaPick/ArenaPick.png", null, { it })
+//    }
+//
+//    @Test
+//    fun testScreenArenaCardPicks() {
+//        recognizePickScreen("ArenaPick/ArenaPick01.png", DHash.SCREEN_ARENA_PICK_01, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick02.png", DHash.SCREEN_ARENA_PICK_02, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick03.png", DHash.SCREEN_ARENA_PICK_03, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick04.png", DHash.SCREEN_ARENA_PICK_04, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick05.png", DHash.SCREEN_ARENA_PICK_05, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick06.png", DHash.SCREEN_ARENA_PICK_06, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick07.png", DHash.SCREEN_ARENA_PICK_07, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick08.png", DHash.SCREEN_ARENA_PICK_08, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick09.png", DHash.SCREEN_ARENA_PICK_09, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick10.png", DHash.SCREEN_ARENA_PICK_10, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick11.png", DHash.SCREEN_ARENA_PICK_11, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick12.png", DHash.SCREEN_ARENA_PICK_12, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick13.png", DHash.SCREEN_ARENA_PICK_13, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick14.png", DHash.SCREEN_ARENA_PICK_14, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick15.png", DHash.SCREEN_ARENA_PICK_15, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick16.png", DHash.SCREEN_ARENA_PICK_16, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick17.png", DHash.SCREEN_ARENA_PICK_17, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick18.png", DHash.SCREEN_ARENA_PICK_18, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick19.png", DHash.SCREEN_ARENA_PICK_19, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick20.png", DHash.SCREEN_ARENA_PICK_20, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick21.png", DHash.SCREEN_ARENA_PICK_21, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick22.png", DHash.SCREEN_ARENA_PICK_22, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick23.png", DHash.SCREEN_ARENA_PICK_23, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick24.png", DHash.SCREEN_ARENA_PICK_24, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick25.png", DHash.SCREEN_ARENA_PICK_25, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick26.png", DHash.SCREEN_ARENA_PICK_26, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick27.png", DHash.SCREEN_ARENA_PICK_27, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick28.png", DHash.SCREEN_ARENA_PICK_28, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick29.png", DHash.SCREEN_ARENA_PICK_29, BufferedImage::getScreenArenaPickNumberCrop)
+//        recognizePickScreen("ArenaPick/ArenaPick30.png", DHash.SCREEN_ARENA_PICK_30, BufferedImage::getScreenArenaPickNumberCrop)
+//    }
+//
+//    @Test
+//    fun testArenaPickClass() {
+//        recognizeArenaPickClass("ArenaClass/PickArcher.png", "Archer")
+//        recognizeArenaPickClass("ArenaClass/PickAssassin.png", "Assassin")
+//        recognizeArenaPickClass("ArenaClass/PickBattlemage.png", "Battlemage")
+//        recognizeArenaPickClass("ArenaClass/PickCrusader.png", "Crusader")
+//        recognizeArenaPickClass("ArenaClass/PickMage.png", "Mage")
+//        recognizeArenaPickClass("ArenaClass/PickMonk.png", "Monk")
+//        recognizeArenaPickClass("ArenaClass/PickScout.png", "Scout")
+//        recognizeArenaPickClass("ArenaClass/PickSorcerer.png", "Sorcerer")
+//        recognizeArenaPickClass("ArenaClass/PickSpellsword.png", "Spellsword")
+//        recognizeArenaPickClass("ArenaClass/PickWarrior.png", "Warrior")
+//    }
+
+    private fun recognizeScreen(testFileName: String, screen: String?, cropFun: (BufferedImage) -> BufferedImage?) {
+        cropFun(getFileImage(testFileName))?.apply {
+            assertThat(recognizeImage(this, DHash.SCREENS_LIST)).isEqualTo(screen)
+        }
+    }
+
+    private fun recognizePickScreen(testFileName: String, screen: String?, cropFun: (BufferedImage) -> BufferedImage?) {
+        cropFun(getFileImage(testFileName))?.apply {
+            assertThat(recognizeImage(this, DHash.SCREENS_PICK_LIST)).isEqualTo(screen)
+        }
+    }
+
+    private fun recognizeArenaPickClass(testFileName: String, cls: String) {
+        val croppedImage = getFileImage(testFileName).getArenaPickClassCrop()
+        croppedImage.saveCroppedImage()
+        assertThat(recognizeImage(croppedImage, DHash.CLASS_PICK_LIST)).isEqualTo(cls)
+    }
+
+    // -- Game Tests --
+
+
+    @Test
+    fun testRing() {
+        var croppedImage = getFileImage("Game/PlayFirst.png").getGamePlayerFirstCrop()
+        assertThat(recognizeImage(croppedImage, DHash.GAME_ITEMS_LIST)).isEqualTo(DHash.PLAYER_GAME_FIRST)
+        croppedImage = getFileImage("Game/PlaySecond.png").getGamePlayerSecondCrop()
+        assertThat(recognizeImage(croppedImage, DHash.GAME_ITEMS_LIST)).isEqualTo(DHash.PLAYER_GAME_SECOND)
+    }
+
+    @Test
+    fun testResult() {
+//        var croppedImage = getFileImage("Game/win.png").getGameWinCrop()
+//        assertThat(recognizeImage(croppedImage, DHash.GAME_ITEMS_LIST)).isEqualTo(DHash.GAME_WIN)
+        var croppedImage = getFileImage("Game/win2.png").getGameWin2Crop()
+        assertThat(recognizeImage(croppedImage, DHash.GAME_ITEMS_LIST)).isEqualTo(DHash.GAME_WIN2)
+//        croppedImage = getFileImage("Game/loss.png").getGameLossCrop()
+//        assertThat(recognizeImage(croppedImage, DHash.GAME_ITEMS_LIST)).isEqualTo(DHash.GAME_LOSS)
+        croppedImage = getFileImage("Game/loss2.png").getGameLoss2Crop()
+        assertThat(recognizeImage(croppedImage, DHash.GAME_ITEMS_LIST)).isEqualTo(DHash.GAME_LOSS2)
+    }
+
+    @Test
+    fun testInitialCardsDraw() {
+        var croppedImage = getFileImage("Game/InitialCardsDraw.png").getGameInitialCardDrawCrop(1)
+        assertThat(recognizeImage(croppedImage, DHashCards.LIST)).isEqualTo("crushingblow")
+        croppedImage = getFileImage("Game/InitialCardsDraw.png").getGameInitialCardDrawCrop(2)
+        assertThat(recognizeImage(croppedImage, DHashCards.LIST)).isEqualTo("preserveroftheroot")
+        croppedImage = getFileImage("Game/InitialCardsDraw.png").getGameInitialCardDrawCrop(3)
+        assertThat(recognizeImage(croppedImage, DHashCards.LIST)).isEqualTo("crushingblow")
+    }
+
+    @Test
+    fun testGameCardDraw() {
+        val croppedImage = getFileImage("Game/CardDraw.png").getGameCardDrawCrop()
+        assertThat(recognizeImage(croppedImage, DHashCards.LIST)).isEqualTo("shadowfenpriest")
+    }
+
+    @Test
+    fun testGameCardDrawProphecy() {
+        val croppedImage = getFileImage("Game/CardProphecy.png").getGameCardDrawProphecyCrop()
+        assertThat(recognizeImage(croppedImage, DHashCards.LIST)).isEqualTo("fightersguildrecruit")
+    }
+
+    @Test
+    fun testGameOpponentClass() {
+        recognizeOpponent("Game/CardDraw.png", "Endurance", BufferedImage::getGameOpponentClassCrop)
+        recognizeOpponent("Game/CardGenerated.png", "Battlemage", BufferedImage::getGameOpponentClassCrop)
+        recognizeOpponent("Game/PlayFirst.png", "Monk", BufferedImage::getGameOpponentClassCrop)
+    }
+
+    @Test
+    fun testGamePlayerClass() {
+        recognizePlayer("Game/CardDraw.png", "Spellsword", BufferedImage::getGamePlayerClassCrop)
+        recognizePlayer("Game/PlayFirst.png", "Assassin", BufferedImage::getGamePlayerClassCrop)
+    }
+
+    private fun recognizeOpponent(testFileName: String, result: String, crop: (BufferedImage) -> BufferedImage) {
+        val croppedImage = crop(getFileImage(testFileName))
+        assertThat(recognizeImage(croppedImage, DHash.GAME_OPPONENT_CLASS_LIST)).isEqualTo(result)
+    }
+
+    private fun recognizePlayer(testFileName: String, result: String, crop: (BufferedImage) -> BufferedImage) {
+        val croppedImage = crop(getFileImage(testFileName))
+        assertThat(recognizeImage(croppedImage, DHash.GAME_PLAYER_CLASS_LIST)).isEqualTo(result)
+    }
+
+    // -- Arena Tests --
+
+//    @Test
+//    fun testArenaPicks() {
+//        recognizeArenaPick("ArenaPick/ArenaPick01.png", "fifthlegiontrainer", "septimguardsman", "snowwolf")
+//        recognizeArenaPick("ArenaPick/ArenaPick02.png", "icespike", "voraciousspriggan", "lightningbolt")
+//        recognizeArenaPick("ArenaPick/ArenaPick03.png", "redoranenforcer", "lightningbolt", "camlornsentinel")
+//        recognizeArenaPick("ArenaPick/ArenaPick04.png", "sparkingspider", "dwarvensphere", "highlandlurcher")
+//        recognizeArenaPick("ArenaPick/ArenaPick05.png", "varaniscourier", "crystaltowercrafter", "fightersguildrecruit")
+//        recognizeArenaPick("ArenaPick/ArenaPick06.png", "drestormentor", "fateweaver", "elixirofdeflection")
+//        recognizeArenaPick("ArenaPick/ArenaPick07.png", "chaurusreaper", "highlandlurcher", "steelsword")
+//        recognizeArenaPick("ArenaPick/ArenaPick08.png", "anxileelinvader", "chaurusreaper", "nimbleally")
+//        recognizeArenaPick("ArenaPick/ArenaPick09.png", "lightningbolt", "reachmanshaman", "arrowintheknee")
+//        recognizeArenaPick("ArenaPick/ArenaPick10.png", "firebolt", "darkharvester", "dunmernightblade")
+//        recognizeArenaPick("ArenaPick/ArenaPick11.png", "nibenbaycutthroat", "nimbleally", "shockingwamasu")
+//        recognizeArenaPick("ArenaPick/ArenaPick12.png", "highrocksummoner", "dunesmuggler", "dreughshellarmor")
+//        recognizeArenaPick("ArenaPick/ArenaPick13.png", "momentofclarity", "greentouchedspriggan", "wildbeastcaller")
+//        recognizeArenaPick("ArenaPick/ArenaPick14.png", "telvanniarcanist", "blacksapprotector", "shriekingharpy")
+//        recognizeArenaPick("ArenaPick/ArenaPick15.png", "icespike", "nibenbaycutthroat", "bardedguar")
+//    }
+
+    private fun recognizeArenaPick(testFileName: String, card1: String, card2: String, card3: String) {
+        val croppedImage1 = getFileImage(testFileName).getArenaCardCrop(1)
+        assertThat(Recognizer.recognizeCardImage(croppedImage1)).isEqualTo(card1)
+        val croppedImage2 = getFileImage(testFileName).getArenaCardCrop(2)
+        assertThat(Recognizer.recognizeCardImage(croppedImage2)).isEqualTo(card2)
+        val croppedImage3 = getFileImage(testFileName).getArenaCardCrop(3)
+        assertThat(Recognizer.recognizeCardImage(croppedImage3)).isEqualTo(card3)
+    }
+
+}
