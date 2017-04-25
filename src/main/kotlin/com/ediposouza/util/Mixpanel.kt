512dd0ccfa98c1c0f06f15eb24fe5b283184ded9
Edipo <edipo2s@gmail.com>
2017-04-24 22:26:09 -0300

    MixPanel base

---
+++ src/main/kotlin/com/ediposouza/util/Mixpanel.kt
@@ -0,0 +1,12 @@
+package com.ediposouza.util
+
+import com.mixpanel.mixpanelapi.MessageBuilder
+
+/**
+ * Created by Edipo on 09/04/2017.
+ */
+object Mixpanel {
+
+    val messageBuilder = MessageBuilder("***REMOVED***")
+
+}
