147a72327612c4d2e8b3e92915d2851fd7a1e65c
Edipo <edipo2s@gmail.com>
2017-05-18 23:03:37 -0300

    Bump version to 0.2.1

--- src/main/kotlin/com/ediposouza/TESLTracker.kt
+++ src/main/kotlin/com/ediposouza/TESLTracker.kt
@@ -63,7 +63,7 @@ class TESLTracker : App(MainStageView::class) {
     companion object {
 
         val APP_NAME = "WabbaTrack"
-        val APP_VERSION = "0.2.0"
+        val APP_VERSION = "0.2.1"
         val DEBUG_FILE_NAME = "WabbaTrack.debug"
         val WABBATRACK_URL = "https://edipo2s.github.io/WabbaTrack/"
 
