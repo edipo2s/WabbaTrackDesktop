4ddb52d2cfefc2ad733c4434b30fc15a897ca73e
Edipo <edipo2s@gmail.com>
2017-05-23 12:41:08 -0300

    Hide deck tracker on arena picks

--- src/main/kotlin/com/ediposouza/executor/ScreenExecutor.kt
+++ src/main/kotlin/com/ediposouza/executor/ScreenExecutor.kt
@@ -81,6 +81,7 @@ object ScreenExecutor {
         launch(CommonPool) {
             if (screenshot.getScreenArenaPicksCrop().matchScreen(PHash.SCREEN_ARENA_PICKS)) {
                 Logger.i("Arena Picks Screen Detected!", true)
+                GameState.hideDeckTracker()
                 StateHandler.currentTESLState = ArenaState.apply {
                     if (finishPicks || TESLTracker.preferences.getBoolean(ARENA_GAME_STARTED_PREF, false)) {
                         TESLTracker.preferences.remove(ARENA_GAME_STARTED_PREF)
